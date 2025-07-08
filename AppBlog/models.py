d2f3a90c232af950f379e88eca0e8da1c3ed4348
enzo <143762380+enzogitcode@users.noreply.github.com>
2025-07-08 20:17:02 -0300

    agregado los templates

--- AppBlog/models.py
+++ AppBlog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+## modelos basados en vistas
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+    
--- AppBlog/views.py
+++ AppBlog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
+# # Modelos basados en vistas
+
+# def home(request):
+#     return render(request, 'AppBlog/home.html')
+
+# class Student:
+#     def __init__(self, name, age, email):
+#         self.name = name
+#         self.age = age
+#         self.email = email
+
+#     def __str__(self):
+#         return f"Se creó el estudiante (name={self.name}, de {self.age} de edad, cuyo email es {self.email})"
+
+# def student_list(request):
+    

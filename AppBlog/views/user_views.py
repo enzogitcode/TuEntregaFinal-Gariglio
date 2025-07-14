a3c504c44ef3cdd44f3cb0197be66978c559921e
enzo <143762380+enzogitcode@users.noreply.github.com>
2025-07-14 05:38:02 -0300

    editando user

--- AppBlog/views/user_views.py
+++ AppBlog/views/user_views.py
@@ -5,12 +5,22 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserChangeForm
 from django.views.generic.edit import UpdateView
 from django.contrib.auth.views import LoginView, LogoutView
-
+from django import forms
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'AppBlog/user/register.html'
     success_url = reverse_lazy('login')
+
+class UserEditForm(UserCreationForm):
+    email= forms.EmailField(label='Modificar el e-mail')
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repetir la contraseña', widget=forms.PasswordInput)
+    last_name = forms.CharField()
+    first_name = forms.CharField()
+    class Meta:
+        #model = User
+        fields = ['email', 'password1', 'password2', 'last_name', 'first_name']
 
 class EditUserView(LoginRequiredMixin, UpdateView):
     form_class = UserChangeForm

0ab986c58605d7d4b16a611dbfba72480b7f0df3
enzo <143762380+enzogitcode@users.noreply.github.com>
2025-07-15 16:40:29 -0300

    se visualiza el avatar pero hay que corregir la view

--- AppBlog/admin.py
+++ AppBlog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Student, Teacher, Article, Paper
+from .models import Student, Teacher, Article, Paper, Avatar
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Article)
 admin.site.register(Paper)
+admin.site.register(Avatar)
--- AppBlog/views/views.py
+++ AppBlog/views/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from AppBlog.models import Avatar
 
+@login_required
 def home(request):
-    return render(request, 'AppBlog/home.html')
+    avatares = Avatar.objects.filter(user=request.user.id)
+    return render(request, 'AppBlog/home.html', {"url": avatares[0].imagen.url})
 
 def about(request):
     return render(request, 'AppBlog/about.html')

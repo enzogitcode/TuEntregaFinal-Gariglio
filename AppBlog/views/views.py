6bb64215502e6f1c5a1a9dbea1de7f29f2c3d6e2
enzo <143762380+enzogitcode@users.noreply.github.com>
2025-07-17 22:06:21 -0300

    otros errores solucionados

--- AppBlog/views/views.py
+++ AppBlog/views/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from AppBlog.models import Avatar
+
 
 def home(request):
     return render(request, 'AppBlog/home.html')
 
 def about(request):
     return render(request, 'AppBlog/about.html')
-
-def forbidden(request):
-    return render(request, 'AppBlog/forbidden.html')
 
 
